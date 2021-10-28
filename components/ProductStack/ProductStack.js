e5e14632f425f41063af5ed423944880f5028cf7
RenarsS <renars.susejs@gmail.com>
2021-10-28 12:21:02 +0300

    map error fixed

--- components/ProductStack/ProductStack.js
+++ components/ProductStack/ProductStack.js
@@ -7,7 +7,7 @@ export default function ProductStack(props) {
         <div className="wrap">
             <Grid container spacing={1} direction="row" justifyContent="center" alignItems="center">
                 {
-                    props.data.map(data => (
+                    props?.data?.map(data => (
                         <Grid item xs={12} sm={6} md={4} lg={3} key={data?.id}>
                             <a href={`/${data?.subcategory?.category?.slug}/${data?.subcategory?.slug}/${data?.slug}`} >
                                 <Product
